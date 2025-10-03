bcfd3aa04310a16b949d801765dde26bf220eea7
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2025-10-04 00:40:54 +0900

    금지어 기능 추가

---
+++ modules/ban_list.js
@@ -0,0 +1,64 @@
+
+// 금지어 리스트 가져오기
+function getForbiddenWords(KV) {
+    let list = KV.get("banlagList");
+    if (!list) {
+        list = [];
+        KV.put("banlagList", list);
+    }
+    return list;
+}
+
+// 금지어 추가
+function addForbiddenWord(msg,KV) {
+    let word="";         // 채팅 내용
+    word=msg.substr(7);   // 명령어 자르고 내용만 담는다.
+
+    let list = getForbiddenWords(KV);
+    if (list.indexOf(word) === -1) {
+        list.push(word);
+        KV.put("banlagList", list);
+        return true;
+    }
+
+    return false;
+}
+
+// 금지어 삭제
+function removeForbiddenWord(msg, KV) {
+    let word = msg.substr(7); // ".금지어삭제 " 명령어 이후 내용만 추출
+    let list = getForbiddenWords(KV);
+
+    let index = list.indexOf(word);
+    if (index !== -1) {
+        list.splice(index, 1); // 해당 단어 삭제
+        KV.put("banlagList", list);
+        return true;
+    }
+    return false;
+}
+
+// 메시지에 금지어 포함 여부 검사
+function containsForbiddenWord(msg,KV) {
+    let list = getForbiddenWords(KV);
+    return list.some(word => msg.includes(word));
+}
+
+// banList에 사용자(sender) 카운트 추가
+function addBanCount(sender,KV) {
+    let list = KV.get("banList") || {}; // 현재 저장된 banList 가져오기
+    if (!list[sender]) {
+        list[sender] = 0; // 처음이면 0으로 초기화
+    }
+    list[sender] += 1; // 카운트 증가
+    KV.put("banList", list); // 저장
+    return list[sender];
+}
+
+
+// export 데이터
+exports.containsForbiddenWord = containsForbiddenWord;
+exports.addBanCount = addBanCount;
+exports.addForbiddenWord = addForbiddenWord;
+exports.getForbiddenWords = getForbiddenWords;
+exports.removeForbiddenWord = removeForbiddenWord;

5bb64fa76ca8c1a59545e7688a76373f1fc300d3
정민호 <sucessmh25@naver.com>
2025-10-04 15:36:53 +0900

    채팅, 금지어 관련 로직 수정

--- watBot.js
+++ watBot.js
@@ -46,7 +46,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
 
     // 채팅 이력 저장
     if(!msg.startsWith(".")){
-        chat_record.addChatCount(sender,KV);
+        chat_record.addChatCount(sender,KV,room);
     }
 
     // 19금 단어 카운팅
@@ -60,46 +60,15 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
         // 중복실행 방지 변수
         nows ='1';
 
-        // 금지어 추가하기
-        if (msg.startsWith(".금지어추가 ")) {
-            replier.reply(ban_list.addForbiddenWord(msg,KV));
+        // 채팅 관련 랭킹
+        if (msg.startsWith(".채팅")) {
+            replier.reply(chat_record.getChatRecordFuntion(msg,KV,room));
         }
-
-        // 채팅 랭킹
-        else if (msg.startsWith(".채팅랭킹")) {
-            let topList = chat_record.getTopChatters(KV);
-            if (topList.length === 0) {
-                replier.reply("오늘 채팅 기록이 없습니다.");
-            } else {
-                let replyText = "오늘 최다 채팅자:\n";
-                topList.forEach(([user, count], index) => {
-                    replyText += `${index+1}. ${user} - ${count}회\n`;
-                });
-                replier.reply(replyText);
-            }
-        }
-        // 월간 채팅 랭킹
-        else if (msg.startsWith(".월채팅랭킹")) {
-            let topList = chat_record.getMonthlyTopChatters(KV);
-            if (topList.length === 0) {
-                replier.reply("이번 달 채팅 기록이 없습니다.");
-            } else {
-                let replyText = "이번 달 최대 채팅자 :\n";
-                topList.forEach(([user, count], index) => {
-                    replyText += `${index+1}. ${user} - ${count}회\n`;
-                });
-                replier.reply(replyText);
-            }
+        // 금지어 관련
+        else if (msg.startsWith(".금지어")) {
+            replier.reply(ban_list.getBanListFuntion(msg,KV));
         }       
-        // 금지어 목록 보기
-        else if (msg.startsWith(".금지어목록")) {
-            replier.reply(ban_list.showForbiddenWords(KV));
-        }
 
-        // 금지어 삭제
-        else if (msg.startsWith(".금지어삭제 ")) {
-            replier.reply(ban_list.removeForbiddenWord(msg,KV));
-        }
         // 왓봇 AI 응답
         else if (msg.startsWith(".챗 ")) {
             replier.reply(ai_data.getAIResponse(msg,apiKey.getApiKey("gpt")));
@@ -115,19 +84,14 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
             replier.reply(station_time_data.getUpcomingTrains(msg));
         }
 
-        // 베이비 데이터
-        else if (msg.startsWith(".아동")) {
-            replier.reply(baby_data.getbabyMsg(msg));
-        }
-
         // 실시간 지하철
         else if (msg.startsWith(".역 ")) {
-            //replier.reply(subway_data.getKoreaSubwayInfo(msg,api_data.getApiKey("subway")));
+            replier.reply(subway_data.getKoreaSubwayInfo(msg,api_data.getApiKey("subway")));
         }
 
         // 실시간 환율
         else if (msg.startsWith(".환율")) {
-           // replier.reply(rate_data.getRate(api_data.getApiKey("rate")));
+            replier.reply(rate_data.getRate(api_data.getApiKey("rate")));
         }
 
         // 버스 노선 불러오기
@@ -138,18 +102,6 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
         // 사용가능한 명령어
         else if ((msg.startsWith(".명령어")) ){
             replier.reply(command_data.getCommandData(msg));
-        }
-
-        // 방장소환
-        else if ((msg.startsWith(".왓")) || (msg.startsWith(".나폴")) || (msg.startsWith(".승겡")) ){
-            replier.reply(command_data.getComeMaster(msg));
-        }
-
-        // 지우개
-        else if ((msg.startsWith(".지우개")) ){
-            for(let i = 0; i < 5; i++) {
-                replier.reply(command_data.getComeMaster(msg));
-            }
         }
 
         // 실시간 날씨

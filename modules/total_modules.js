e353a391d2799236bcfa4656d53d5d5b59bd09df
정민호 <sucessmh25@naver.com>
2025-10-04 16:28:22 +0900

    모듈 로직 분리

---
+++ modules/total_modules.js
@@ -0,0 +1,14 @@
+module.exports = {
+    station_time_data: require('./station_time_data.js'),
+    ai_data: require('./ai.js'),
+    deepL_data: require('./deepL.js'),
+    subway_data: require('./subway_data.js'),
+    baby_data: require('./baby_data.js'),
+    rate_data: require('./rate_data.js'),
+    bus_data: require('./bus_data.js'),
+    command_data: require('./command_data.js'),
+    ban_list: require('./ban_list.js'),
+    chat_record: require('./chat_record.js'),
+    apiKey: require('./api_key_list.js'),
+    RhinoKV: require('RhinoKV')
+};

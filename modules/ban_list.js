18b8e6cdde3dd702f0ecd3644ea3d8a8c809b983
정민호 <sucessmh25@naver.com>
2025-10-04 16:15:19 +0900

    금지단어 로직 분리

--- modules/ban_list.js
+++ modules/ban_list.js
@@ -1,87 +1,165 @@
+const forbidden_words_list_19 = "forbidden_words_list_19";
+const forbidden_words_sender_list_19 = "forbidden_words_sender_list_19";
+const forbidden_words_list = "forbidden_words_list";
+const forbidden_words_sender_list = "forbidden_words_sender_list";
+
+
 // 금지어 관련 함수 분기
 function getBanListFuntion(msg,KV) {
 
-    // 금지어 추가
-    if (msg.startsWith(".금지어추가 ")) {
-        return addForbiddenWord(msg,KV);
-    }       
+    // 19금지어 추가
+    if (msg.startsWith(".금지어19추가 ")) {
+        return addForbiddenWord(msg,KV,"19");
+    }
+    else if (msg.startsWith(".금지어추가 ")) {
+        return addForbiddenWord(msg,KV,null);
+    }         
     // 금지어 목록 보기
     else if (msg.startsWith(".금지어목록")) {
-        return showForbiddenWords(KV);
+        return showForbiddenWords(KV,null);
     }
-
+        //19금지어 목록 보기
+    else if (msg.startsWith(".금지어19목록")) {
+        return showForbiddenWords(KV,"19");
+    }
     // 금지어 삭제
     else if (msg.startsWith(".금지어삭제 ")) {
-        return removeForbiddenWord(msg,KV);
+        return removeForbiddenWord(msg,KV,"19");
     }
-}
-
-// 금지어 리스트 가져오기
-function getForbiddenWords(KV) {
-    let list = KV.get("banlagList");
-    if (!list) {
-        list = [];
-        KV.put("banlagList", list);
+    // 19금지어 삭제
+    else if (msg.startsWith(".금지어19삭제 ")) {
+        return removeForbiddenWord(msg,KV,null);
     }
-    return list;
 }
 
 // 금지어 목록 보기
-function showForbiddenWords(KV) {
-    let list = getForbiddenWords(KV);
+function showForbiddenWords(KV,gubun) {
+    let list = getForbiddenWords(KV,gubun);
+    let name;
+    if(gubun == "19"){
+        name ="금지된 19금 단어목록"
+    }else{
+        name ="금지어 목록"
+    }
     if (list.length === 0) {
-        return "현재 금지어 목록이 비어 있습니다.";
+        return "현재 "+name+"이 비어 있습니다.";
     } else {
-        return "현재 금지어 목록: " + list.join(", ");
+        return "현재 "+name+" : " + list.join(", ");
     }
 }
 
 // 금지어 추가
-function addForbiddenWord(msg,KV) {
-    let word="";         // 채팅 내용
-    word=msg.substr(7);   // 명령어 자르고 내용만 담는다.
+function addForbiddenWord(msg,KV,gubun) {
+    if(gubun == "19"){
 
-    let list = getForbiddenWords(KV);
-    if (list.indexOf(word) === -1) {
-        list.push(word);
-        KV.put("banlagList", list);
-        return "금지어 추가 성공";
+        let word="";         // 채팅 내용
+        word=msg.substr(9);  // 명령어 자르고 내용만 담는다.
+        let list = getForbiddenWords(KV,gubun);
+
+        if (list.indexOf(word) === -1) {
+            list.push(word);
+            KV.put(forbidden_words_list_19, list);
+            return "금지어 추가 성공";
+        }
+
+        return "금지어 추가 실패";
+
+    }else{
+
+        let word="";         // 채팅 내용
+        word=msg.substr(7);   // 명령어 자르고 내용만 담는다.
+        let list = getForbiddenWords(KV,gubun);
+
+        if (list.indexOf(word) === -1) {
+            list.push(word);
+            KV.put(forbidden_words_list, list);
+            return "금지어 추가 성공";
+        }
+
+        return "금지어 추가 실패";
     }
-
-    return "금지어 추가 실패";
 }
 
 // 금지어 삭제
-function removeForbiddenWord(msg, KV) {
-    let word = msg.substr(7); // ".금지어삭제 " 명령어 이후 내용만 추출
-    let list = getForbiddenWords(KV);
+function removeForbiddenWord(msg, KV, gubun) {
+    if(gubun == "19"){
+        let word = msg.substr(9); // ".금지어삭제 " 명령어 이후 내용만 추출
+        let list = getForbiddenWords(KV,gubun);
 
-    let index = list.indexOf(word);
-    if (index !== -1) {
-        list.splice(index, 1); // 해당 단어 삭제
-        KV.put("banlagList", list);
-        return "금지어 삭제 성공";
+        let index = list.indexOf(word);
+        if (index !== -1) {
+            list.splice(index, 1); // 해당 단어 삭제
+            KV.put(forbidden_words_list_19, list);
+            return "19 금지어 삭제 성공";
+        }
+        return "19 금지어 삭제 실패";
+    }else{
+        let word = msg.substr(7); // ".금지어삭제 " 명령어 이후 내용만 추출
+        let list = getForbiddenWords(KV,gubun);
+
+        let index = list.indexOf(word);
+        if (index !== -1) {
+            list.splice(index, 1); // 해당 단어 삭제
+            KV.put(forbidden_words_list, list);
+            return "금지어 삭제 성공";
+        }
+        return "금지어 삭제 실패";
     }
-    return "금지어 삭제 실패";
 }
 
-// 메시지에 금지어 포함 여부 검사
-function containsForbiddenWord(msg,KV) {
-    let list = getForbiddenWords(KV);
+// 메시지에 19금 & 금지어 포함 여부 검사
+function containsForbiddenWord(msg,KV,gubun) {
+    let list = getForbiddenWords(KV,gubun);
     return list.some(word => msg.includes(word));
 }
 
-// banList에 사용자(sender) 카운트 추가
-function addBanCount(sender,KV) {
-    let list = KV.get("banList") || {}; // 현재 저장된 banList 가져오기
-    if (!list[sender]) {
-        list[sender] = 0; // 처음이면 0으로 초기화
+// 금지어 리스트 가져오기
+function getForbiddenWords(KV,gubun) {
+    let list;
+    if(gubun == "19"){
+        list = KV.get(forbidden_words_list_19);
+    }else{
+        list = KV.get(forbidden_words_list);
     }
-    
-    list[sender] += 1; // 카운트 증가
-    KV.put("banList", list); // 저장
 
-    return sender + "님 19금 단어 사용: "+list[sender]+"회";
+    if (!list) {
+        list = [];
+        if(gubun == "19"){
+            KV.put(forbidden_words_list_19, list);
+        }else{
+            KV.put(forbidden_words_list, list);
+        }
+    }
+    return list;
+}
+
+// banList에 사용자(sender) 카운트 추가
+function addBanCount(sender,KV,gubun) {
+
+    if(gubun == "19"){
+        let list = KV.get(forbidden_words_sender_list_19) || {}; // 현재 저장된 banList 가져오기
+        
+        if (!list[sender]) {
+            list[sender] = 0; // 처음이면 0으로 초기화
+        }
+        
+        list[sender] += 1; // 카운트 증가
+        KV.put(forbidden_words_sender_list_19, list); // 저장
+
+        return sender + "님 19금 단어 사용: "+list[sender]+"회";
+
+    }else{
+
+        let list = KV.get(forbidden_words_sender_list) || {}; // 현재 저장된 banList 가져오기
+        if (!list[sender]) {
+            list[sender] = 0; // 처음이면 0으로 초기화
+        }
+        
+        list[sender] += 1; // 카운트 증가
+        KV.put(forbidden_words_sender_list, list); // 저장
+
+        return sender + "님 정치 단어 사용: "+list[sender]+"회";
+    }
 }
 
 
